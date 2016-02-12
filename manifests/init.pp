48da7267a69b7a31e06e5577646b1be5ac5beb6a
oronb <orong1234@gmail.com>
2016-02-13 01:15:47 +0200

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 # == Class: artifactory
 #
-# This class installs and configures artifactory, apache vhost, and backups.
+# This class installs and configures artifactory
 #
 #
 # === Parameters
@@ -9,11 +9,6 @@
 #   String.  Version of artifactory to be installed or latest/present
 #   Default: latest
 #
-# [*serverAlias*]
-#   String of comman seperated hostnames or array of hostnames.
-#   Default: artifactory
-#
-#
 # === Examples
 #
 # * Installation:
@@ -21,7 +16,7 @@
 #
 #
 # === Authors
-#
+#   Oron Bortman <oronb:orong1234@gmail.com
 # * Justin Lambert <mailto:jlambert@letsevenup.com>
 #
 class artifactory(
